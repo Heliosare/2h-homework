710f60507124ab87f9262214e5ecfb67c4d7e66c
Vincent Galoger <vincent.galoger@brennus-analytics.com>
2020-10-28 17:05:55 +0100

    add assignee user in list-ticket

--- src/app/list-tickets/list-tickets.component.ts
+++ src/app/list-tickets/list-tickets.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Ticket } from 'src/interfaces/ticket.interface';
 import { User } from 'src/interfaces/user.interface';
 import { BackendService } from '../backend.service';
@@ -10,13 +10,32 @@ import { BackendService } from '../backend.service';
   templateUrl: './list-tickets.component.html',
   styleUrls: ['./list-tickets.component.css']
 })
-export class ListTicketsComponent {
-
+export class ListTicketsComponent implements OnInit, OnDestroy {
   public readonly users$: Observable<User[]> = this.backendService.users();
+  suscriptionUsers: Subscription;
   public readonly tickets$: Observable<Ticket[]> = this.backendService.tickets();
   public users: User[];
 
-  constructor(private readonly backendService: BackendService, private router: Router) {}
+  constructor(private readonly backendService: BackendService) {}
 
+  ngOnInit(): void {
+    this.users$.subscribe((users) => {
+      this.users = users;
+      console.log('users', users);
+    })
+  }
 
+  findUserAssigned(assigneeId) {
+    if (this.users && assigneeId) {
+      const res = this.users.find((user: User) => user.id === assigneeId);
+      return res.name;
+    }
+    return '';
+  }
+
+  ngOnDestroy(){
+    if (this.suscriptionUsers) {
+      this.suscriptionUsers.unsubscribe();
+    }
+  }
 }
